a77a31bec0c53ffe152e59246b20fcc05a64c644
Rene Fabian <loneyzafiroro@gmail.com>
2020-07-03 23:19:48 -0500

    03/07/2020

--- js/enviar_datos_solicitud.js
+++ js/enviar_datos_solicitud.js
@@ -1,5 +1,5 @@
 function postdatos() {
-    let url = `https://192.168.1.68:8080/alcaldiamp/statistics`;
+    let url = `https://192.168.1.68:8080/request`;
     var data = {}
     data.nombre = document.getElementById('nombre').value;
     data.paterno = document.getElementById('paterno').value;
@@ -12,11 +12,29 @@ function postdatos() {
     data.cp = document.getElementById('cp').value;
     data.colonia = document.getElementById('colonia').value;
     data.pueblo = document.getElementById('pueblo').value;
-    data.tipo_solicitud = document.getElementById('pueblo').value;
+    data.tipo_solicitud = document.getElementById('tipo_solicitud').value;
     data.descripcion = document.getElementById('descripcion').value;
+
+    
+    //  data.nombre = "rene";
+
     let datos = JSON.stringify(data);
     const api = new XMLHttpRequest();
     api.open('POST', url, true);
     api.send(datos);
+
+
+//     var url = 'http://192.168.1.68:8080/request';
+//     var data = {username: 'ejemplo'};
+
+// fetch(url, {
+//   method: 'POST', // or 'PUT'
+//   body: JSON.stringify(data), // data can be `string` or {object}!
+//   headers:{
+//     'Content-Type': 'application/json'
+//   }
+// }).then(res => res.json())
+// .catch(error => console.error('Error:', error))
+// .then(response => console.log('Success:', response));
 
 }
