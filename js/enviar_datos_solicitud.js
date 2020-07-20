a157860828fa540d1324fee0952c7b9ff83161ad
Gato con lentes <64568226+Rene-Fabian@users.noreply.github.com>
2020-07-20 17:14:31 -0500

    20/07/2020

--- js/enviar_datos_solicitud.js
+++ js/enviar_datos_solicitud.js
@@ -1,36 +1,34 @@
-// document.querySelector('#enviar').addEventListener('click', function(){
+// document.querySelector('enviar').addEventListener('click', function(){
 // 	postdatos();
 // });
+
 function postdatos() {
-    const url = 'http://localhost:2727/request';
+    var url = 'http://localhost:2727/request';
     var data = {};
     data.nombre = document.getElementById('nombre').value;
     data.paterno = document.getElementById('paterno').value;
     data.materno = document.getElementById('materno').value;
     data.correo = document.getElementById('correo').value;
-    data.telefono = document.getElementById('telefono').value;
     data.sexo = document.getElementById('sexo').value;
+    data.telefono = document.getElementById('telefono').value;
     data.calle = document.getElementById('calle').value;
     data.interior = document.getElementById('interior').value;
     data.exterior = document.getElementById('exterior').value;
     data.cp = document.getElementById('cp').value;
     data.colonia = document.getElementById('colonia').value;
     data.village = document.getElementById('poblado').value;
-    data.tipo_solicitud = document.getElementById('tipo_solicitud').value;
+    // data.tipo_solicitud = document.getElementById('tipo_solicitud').value;
     data.descripcion = document.getElementById('descripcion').value;
     // create request object
-    const request = new Request(url, {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: new Headers({
-        'Content-Type': 'application/json'
-    })
-});
-// pass request object to `fetch()`
-fetch(request)
-    .then(res => res.json())
-    .then(res => console.log(res));
-    
+    console.log(data);
+
+    var json = JSON.stringify(data);
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+   
+    xhr.send(json);
+
 }
 
 
