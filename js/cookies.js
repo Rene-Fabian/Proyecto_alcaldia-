39eb156e2dc4576a25ba768cdc4b3379685456c0
Gato con lentes <64568226+Rene-Fabian@users.noreply.github.com>
2020-08-12 13:05:54 -0500

    cookies

--- js/cookies.js
+++ js/cookies.js
@@ -1,1 +1,11 @@
-document.cookie = 'cross-site-cookie=bar; SameSite=None; Secure';
+var nombre = document.getElementById('nombre');
+var paterno = document.getElementById('paterno');
+var materno = document.getElementById('materno');
+var tipo_solicitud = document.getElementById('tipo_solicitud');
+var descripcion = document.getElementById('descripcion');
+
+document.cookie =  nombre;
+document.cookie =  paterno;
+document.cookie =  materno
+document.cookie =  tipo_solicitud;
+document.cookie =  descripcion;
